de4a55957886285514594cdf87f0c54d0a9afdf1
Federico Urbani <urbanifederico5@gmail.com>
2023-01-23 17:33:01 +0100

    inputoutput4update

---
+++ inputoutput-04/src/PrintYourNameAndPi.java
@@ -0,0 +1,6 @@
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+
+        System.out.printf(String.format("%s%nThe number Pi is: %f","FEDERICO",3.141590));
+    }
+}
